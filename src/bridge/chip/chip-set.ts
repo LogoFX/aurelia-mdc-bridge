be30e9188be8797d164d951244f11748259b5ea2
David Kossoglyad <david@logoui.co.il>
2018-12-27 14:04:06 +0200

    fix (mdc-chip-set)

--- src/bridge/chip/chip-set.ts
+++ src/bridge/chip/chip-set.ts
@@ -1,6 +1,9 @@
 import { MDCChipSet } from '@material/chips';
 import { getLogger, Logger } from 'aurelia-logging';
+import { autoinject, customElement } from 'aurelia-framework';
 
+@customElement('mdc-chip-set')
+@autoinject
 export class MdcChipSet {
   private log: Logger;
   private chipSetElement: HTMLDivElement;
@@ -8,13 +11,16 @@ export class MdcChipSet {
 
   constructor(private element: Element) {
     this.log = getLogger('mdc-chip-set');
+    // tslint:disable-next-line:no-console
+    console.log(element);
   }
 
   private bind() { /** */ }
   private unbind() { /** */ }
 
   private attached() {
-    console.log("MdcChipSet attached()")
+    // tslint:disable-next-line:no-console
+    console.log('MdcChipSet attached() - {0}', this.chipSetElement);
     this.mdcElement = new MDCChipSet(this.chipSetElement);
   }
 
